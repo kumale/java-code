0678aa382ce16d488cb50a6a673e6697f451c670
kumale <168124700+kumale@users.noreply.github.com>
2024-10-15 14:49:23 +0530

    Upload Gk1 file

---
+++ Gk1.java
@@ -0,0 +1,16 @@
+
+public class Gk1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int t=30;
+         int u=40;
+         int v=60;
+         int w=47;
+         int y=96;
+         int z=(t+u+v+w+y)/5;
+         System.out.println(z);
+         
+	}
+
+}
